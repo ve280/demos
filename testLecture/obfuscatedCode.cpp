4b88ba4e9ae1ca6f4e3c716b480f73abd59c8597
Paul Weng <paulweng@Pauls-MacBook-Air.local>
2020-05-14 17:36:42 +0800

    added files for test lecture

---
+++ testLecture/obfuscatedCode.cpp
@@ -0,0 +1,14 @@
+    #include <stdio.h>
+    int main()
+    {
+        int a=5,b=6,c=10,Q,d=16,e=47;
+        for (b=e=0xA;Q="~YoYo--Hwz-Lyf=Tough(&ptr)(Woo)(add+all)\
+        YDt!ULo RCBGCDk RBMDk RBMDk RCBBBCBEk RBADADBDl+\
+        QCEAGDk RBDCGBl SBBFEBm TBJBn VJp XFr-\
+        [At HCBDBBABBCBBCBCBBC` GBABABBABAAABBABABAAABBADBa+\
+        GBABADDACBABABCBCBBC` [At LBABBCBCBCBBBBh LEABABADADCBj$\
+        LBABABABABCBEBj [At KDBBABEBBCBBCBe KBABABBBAAABBBABABCBe KDBBCBABCBABADABe" [b+++30]; ) 
+         for(; Q--> 0x40 ; )
+         putchar ( ++e=='Z' ? e =0xA:041^b&1); 
+         return 0x0;
+    }
