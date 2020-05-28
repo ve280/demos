1045586b98aece7a67aa16f5d900c89da62d2693
Paul Weng <paulweng@Pauls-MacBook-Air.local>
2020-05-28 12:05:49 +0800

    return function pointer example

--- L7_function/fp.cpp
+++ L7_function/fp.cpp
@@ -17,6 +17,16 @@ int max(int x, int y){
     }
 }
 
+typedef int (* funp_t)(int, int);
+
+funp_t choose(int i){
+    if (i==0){
+        return min;
+    } else{
+        return max;
+    }
+}
+
 int main(){
     int (* fp)(int, int) = min;
     int (* fpArray[])(int, int) = {min, max};
@@ -24,10 +34,13 @@ int main(){
     cout << fp(1, 2) << endl;
     cout << fpArray[0](2, 3) << endl;
     cout << fpArray[1](3, 4) << endl;
-    fp++;
-    fpArray++;
+    //fp++;
+    //fpArray++;
     fpp++;
     cout << (*fpp)(4, 5) << endl;
     cout << (void *)fp << endl;
+
+    cout << choose(0)(6, 7) << endl;
+    cout << choose(1)(6, 7) << endl;
     return 0;
 }
